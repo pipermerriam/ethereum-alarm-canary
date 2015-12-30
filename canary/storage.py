c070fa9362e10f44d21c36dbf6ec9b026d04e481
Piper Merriam <pipermerriam@gmail.com>
2015-12-30 13:21:05 -0700

    update to have both storages available

--- canary/storage.py
+++ canary/storage.py
@@ -1,9 +1,16 @@
-from django.contrib.staticfiles.storage import CachedFilesMixin
+from django.contrib.staticfiles.storage import (
+    CachedFilesMixin,
+    ManifestFilesMixin,
+)
 
 from pipeline.storage import PipelineMixin
 
 from s3_folder_storage.s3 import StaticStorage
 
 
-class S3PipelineStorage(PipelineMixin, CachedFilesMixin, StaticStorage):
+class S3PipelineCachedStorage(PipelineMixin, CachedFilesMixin, StaticStorage):
+    pass
+
+
+class S3PipelineManifestStorage(PipelineMixin, ManifestFilesMixin, StaticStorage):
     pass
